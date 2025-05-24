a6c211bea7dea3d6e0fbf9374598e78b18c053df
baielbekenov <baelbekbolotovb@gmail.com>
2025-05-24 17:10:37 +0600

    'addedelective'

--- konditer/main/settings.py
+++ konditer/main/settings.py
@@ -136,7 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "static/"
 
 # Extra places for collectstatic to find static files.
