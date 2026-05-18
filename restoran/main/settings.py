e9dd6672f428814dd5c1c801978ac17bac0f68ea
baielbekenov <baelbekbolotovb@gmail.com>
2026-05-18 08:47:28 +0600

    ssss

--- restoran/main/settings.py
+++ restoran/main/settings.py
@@ -138,7 +138,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-CSRF_TRUSTED_ORIGINS = ["https://*.ngrok-free.app", "https://*.ngrok-free.dev", "https://trust-currencies-wearing-diamond.trycloudflare.com"]
+CSRF_TRUSTED_ORIGINS = ["https://*.ngrok-free.app", "https://*.ngrok-free.dev", "https://medicines-separated-richard-joins.trycloudflare.com/", "https://trust-currencies-wearing-diamond.trycloudflare.com"]
 
 TELEGRAM_BOT_TOKEN = "8461162231:AAHjGse28mc2fghjHKej1asAzPA3TH_zsWI"
 
